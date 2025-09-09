c410364ebf88fd6b501003469c8239c10ed2371b
Lingfeng Wang <62014693+wlf-darkmatter@users.noreply.github.com>
2025-09-09 11:03:04 +0800

    [rollout] chore: Add enable_prefix_caching into config (#3395)
    ### What does this PR do?
    
    Added enable_prefix_caching to the RolloutConfig. This feature provides
    no significant performance benefit in short-input-long-output scenarios
    (e.g., 2k input to 34k output) and occupies a certain amount of device
    memory.
    
    ### Checklist Before Starting
    
    - [x] Search for similar PRs. Paste at least one query link here: ...
    - [x] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `sglang`, `vllm`, `rollout`,
    `trainer`, `ci`, `training_utils`, `recipe`, `hardware`, `deployment`,
    `ray`, `worker`, `single_controller`, `misc`, `perf`, `model`, `algo`,
    `env`, `tool`, `ckpt`, `doc`, `data`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    
    ### API and Usage Example
    
    to disable enable_prefix_caching, using
    
    ```bash
    actor_rollout_ref.rollout.enable_prefix_caching=False 
    ```
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [x] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [x] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [x] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)

--- verl/workers/config/rollout.py
+++ verl/workers/config/rollout.py
@@ -156,6 +156,9 @@ class RolloutConfig(BaseConfig):
     profiler: Optional[ProfilerConfig] = None
 
     enable_chunked_prefill: bool = True
+
+    enable_prefix_caching: bool = True
+
     load_format: str = "dummy_dtensor"
 
     layered_summon: bool = False
--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -194,7 +194,7 @@ class vLLMRollout(BaseRollout):
             disable_log_stats=config.disable_log_stats,
             max_num_batched_tokens=max_num_batched_tokens,
             enable_chunked_prefill=config.enable_chunked_prefill,
-            enable_prefix_caching=True,
+            enable_prefix_caching=config.enable_prefix_caching,
             trust_remote_code=trust_remote_code,
             seed=config.get("seed", 0),
             **compilation_config,
